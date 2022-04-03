ceeeb16e5eb93e43880bdcee172e12c0a4615b84
Theryston Santos <funktodo2@gmail.com>
2022-04-03 20:41:51 -0300

    refactor: 💡 remove interface IPalette

--- @types/styled.d.ts
+++ @types/styled.d.ts
@@ -1,9 +1,5 @@
 // styled.d.ts
 import "styled-components";
-interface IPalette {
-  main: string;
-  contrastText: string;
-}
 
 declare module "styled-components" {
   export interface DefaultTheme {
